f44b173f605d3f9e2ecf05f41d7891319d1dc62a
Al3x34 <sa.castrov@uea.edu.ec>
2026-01-25 15:06:09 -0500

    Tarea de pilas terminada con menu

--- Week 7/Ejercicio 1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Week 7/Ejercicio 1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ed8cfb3bb03f303be66fc73fe8fc7c53870880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76982d5f24325652899e3941fa66198fff66d1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
